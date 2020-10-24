576f86a641101d69501b480ed3a3b8d0c505200d
Duy Do <danny@imprint.com>
2020-10-23 23:18:39 -0500

    create category and question migration

---
+++ database/migrations/2020_10_24_035600_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('category_id');
+            $table->string('question');
+            $table->text('answer');
+            $table->string('audio_question_path');
+            $table->string('audio_answer_path');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
